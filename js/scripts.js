21ce60906a979d9facdc7f2a27b9123b5494dba0
Makiyo <56912851+makiyoR@users.noreply.github.com>
2020-01-13 13:19:49 +0200

    added some test javascript

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+console.log('that was it');
+
+var a = 1;
+var b = 2;
+
+if (a < b){
+    console.log('a is lesser than b');
+} else{
+    console.log('a is greater than b');
+}
+
+if (a === 1){
+    console.log('a is exactly 1');
+}
+
+/*
+if (a < b) {
+    // do the following things if a is lesser than b
+    console.log('a is lesser than b');
+    console.log(a + ' is lesser than ' + b);
+} else {
+    // do the following things otherwise
+    console.log('a is not greater than b');
+}
+*/
+
+
