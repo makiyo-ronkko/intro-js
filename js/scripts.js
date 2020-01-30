b1c878387a91afa89e2199773a40c708094145e6
Makiyo <56912851+makiyoR@users.noreply.github.com>
2020-01-30 13:36:17 +0200

    jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var registeredUsers = []; // this array stores valid usernames until the next pageload
 
+$(document).ready(function () {
+  $('#registerBtn').on('click', validateForm);
+});
+
 function validateForm(e) {
   e.preventDefault(); // stop the submit button from refreshing the page
   console.log("validating....");
@@ -60,12 +64,14 @@ function getUserDataObj() {
 }
 
 function renderRegisteredUsers() {
-  document.getElementById("registered-users").innerHTML = '';
+  //document.getElementById("registered-users").innerHTML = '';
+  $('#registered-users').empty();
 
   registeredUsers.forEach(function (registeredUser) {
-    var _newUser = document.createElement("li");
-    _newUser.innerHTML = JSON.stringify(registeredUser);
-    document.getElementById("registered-users").appendChild(_newUser);
+    //var _newUser = document.createElement("li");
+    //_newUser.innerHTML = JSON.stringify(registeredUser);
+    //document.getElementById("registered-users").appendChild(_newUser);
+    $('<li>').text(JSON.stringify(registeredUsers)).appendTo('#registered-users');
   });
 }
 
@@ -207,37 +213,40 @@ function validatePhoneNumber() {
  * @returns [Boolean] true when valid, false otherwise
  */
 function getUserName() {
-  if (typeof (document.registration.username.value) === "undefined") {
+  if (//typeof (document.registration.username.value) === "undefined")
+    typeof ($('[name="username"]')).val() === 'undefined') {
     return "";
   } else {
-    return document.registration.username.value;
+    //return document.registration.username.value;
+    return $('[name="username"]').val();
   }
 }
 
 function getEmail() {
   // TODO
-  if (typeof (document.registration.email.value) === "undefined") {
+  if (//typeof (document.registration.email.value) === "undefined") 
+    typeof ($('[name="email"]')).val() === 'undefined') {
     return "";
   } else {
-    return document.registration.email.value;
+    return $('[name="email"]').val();
   }
 }
 
 function getPassword() {
   // TODO
-  if (typeof (document.registration.password.value) === "undefined") {
+  if (typeof ($('[name="password"]')).val() === 'undefined') {
     return "";
   } else {
-    return document.registration.password.value;
+    return $('[name="password"]').val();
   }
 }
 
 function getConfirmPassword() {
   // TODO
-  if (typeof (document.registration.password_confirm.value) === "undefined") {
+  if (typeof ($('[name="password_confirm"]')).val() === 'undefined') {
     return "";
   } else {
-    return document.registration.password_confirm.value;
+    return $('[name="password_confirm"]').val();
   }
 }
 
@@ -245,27 +254,27 @@ function getConfirmPassword() {
 
 function getFirstName() {
   // TODO
-  if (typeof (document.registration.firstname.value) === "undefined") {
+  if (typeof ($('[name="firstname"]')).val() === 'undefined') {
     return "";
   } else {
-    return document.registration.firstname.value;
+    return $('[name="firstname"]').val();
   }
 }
 
 function getLastName() {
   // TODO
-  if (typeof (document.registration.lastname.value) === "undefined") {
+  if (typeof ($('[name="lastname"]')).val() === 'undefined') {
     return "";
   } else {
-    return document.registration.lastname.value;
+    return $('[name="lastname"]').val();
   }
 }
 
 function getPhoneNumber() {
   // TODO
-  if (typeof (document.registration.phonenumber.value) === "undefined") {
+  if (typeof ($('[name="phonenumber"]')).val() === 'undefined') {
     return "";
   } else {
-    return document.registration.phonenumber.value;
+    return $('[name="phonenumber"]').val();
   }
 }
