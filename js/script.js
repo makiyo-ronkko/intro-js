47c0f4f40323d4492bca5c0d8e0f73084becaced
makiyo-ronkko <56912851+makiyo-ronkko@users.noreply.github.com>
2020-02-13 19:20:59 +0200

    audioplay

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ function moveThoseArms() {
     var hr = now.getHours();
     var minutes = now.getMinutes();
     var sec = now.getSeconds();
-    //var tick = 360 / 60;
 
     console.log(hr, minutes, sec);
 
@@ -19,11 +18,6 @@ function moveThoseArms() {
     var MINUTEHAND = document.querySelector('#minute');
     var SECONDHAND = document.querySelector('#second');
 
-
-    //secondArmPosition += tick; // second arm moves extra 6/60 deg every sec
-    //minuteArmPosition += tick / 60; // minute arm moves extra 6/60 deg every sec
-    //hourArmPosition += tick / 60 / 60; // hour arm moves extra 6/60 deg every sec
-
     HOURHAND.style.transform = "rotate(" + deltaHr + "deg)";
     MINUTEHAND.style.transform = "rotate(" + deltaMin + "deg)";
     SECONDHAND.style.transform = "rotate(" + deltaSec + "deg)";
@@ -33,13 +27,23 @@ function moveThoseArms() {
 var interval = setInterval(moveThoseArms, 1000);
 
 /*
-setInterval(function () {
-    deltaSec += 6;
-    deltaMin = deltaSec / 60;
-    deltaHr = deltaMin / 60;
-
-    HOURHAND.style.transform = "rotate(" + deltaHr + "deg)";
-    MINUTEHAND.style.transform = "rotate(" + deltaMin + "deg)";
-    SECONDHAND.style.transform = "rotate(" + deltaSec + "deg)";
-}, 1000);
+audio play
 */
+
+var x = document.getElementById("myAudio");
+var audioHour = document.getElementById("hour-music");
+var audioMinute = document.getElementById("minute-music");
+
+var now = new Date();
+var hr = now.getHours();
+var minutes = now.getMinutes();
+
+function playAudio() {
+    if (audioHour == hr && audioMinute == minutes) {
+        x.play();
+    } else (console.log(alert('time is wrong!')));
+}
+
+function pauseAudio() {
+    x.pause();
+}
