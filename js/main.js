cbffe708182fca72311f9cfbf3b7bd00e0e7d610
Makiyo <56912851+makiyoR@users.noreply.github.com>
2020-02-27 10:48:47 +0200

    popup1 visible

--- js/main.js
+++ js/main.js
@@ -1,38 +1,45 @@
-(function($) {
+(function ($) {
 
-	'use strict';
+  'use strict';
 
-	// bootstrap dropdown hover
+  // bootstrap dropdown hover
 
   // loader
-  var loader = function() {
-    setTimeout(function() { 
-      if($('#loader').length > 0) {
+  var loader = function () {
+    setTimeout(function () {
+      if ($('#loader').length > 0) {
         $('#loader').removeClass('show');
       }
     }, 1);
   };
   loader();
 
-	
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			$this.find('.dropdown-menu').removeClass('show');
-	});
+  // popup1 visibility
+
+  function overlay() {
+    var overlay = document.getElementById("popup1");
+    overlay.style.visibility = "visible";
+  }
+  overlay();
+
+  $('nav .dropdown').hover(function () {
+    var $this = $(this);
+    $this.addClass('show');
+    $this.find('> a').attr('aria-expanded', true);
+    $this.find('.dropdown-menu').addClass('show');
+  }, function () {
+    var $this = $(this);
+    $this.removeClass('show');
+    $this.find('> a').attr('aria-expanded', false);
+    $this.find('.dropdown-menu').removeClass('show');
+  });
 
 
   var offcanvas_toggle = $('.js-offcanvas-toggle');
-  offcanvas_toggle.on('click', function() {
+  offcanvas_toggle.on('click', function () {
 
 
-    if ( $('body').hasClass('offcanvas-open') ) {
+    if ($('body').hasClass('offcanvas-open')) {
       $('body').removeClass('offcanvas-open');
     } else {
       $('body').addClass('offcanvas-open');
@@ -41,84 +48,84 @@
   });
 
 
-  $(document).click(function(e) {
+  $(document).click(function (e) {
     var container = $('.js-offcanvas-toggle, #offcanvas_menu');
     if (!container.is(e.target) && container.has(e.target).length === 0) {
-      if ( $('body').hasClass('offcanvas-open') ) {
+      if ($('body').hasClass('offcanvas-open')) {
         $('body').removeClass('offcanvas-open');
       }
     }
   });
 
 
-  $('#date-countdown').countdown('2020/10/10', function(event) {
-  var $this = $(this).html(event.strftime(''
-    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
-    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
-    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
-    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
-    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
-});
-
-	// home slider
-	$('.home-slider').owlCarousel({
-    loop:true,
-    autoplay: true,
-    margin:10,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav:true,
-    autoplayHoverPause: false,
-    items: 1,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:1,
-        nav:false
-      },
-      1000:{
-        items:1,
-        nav:true
-      }
-    }
-	});
-
+  $('#date-countdown').countdown('2020/10/10', function (event) {
+    var $this = $(this).html(event.strftime(''
+      + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
+      + '<span class="countdown-block"><span class="label">%d</span> days </span>'
+      + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
+      + '<span class="countdown-block"><span class="label">%M</span> min </span>'
+      + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+  });
 
   // home slider
-  $('.testimony-slider').owlCarousel({
-    loop:true,
+  $('.home-slider').owlCarousel({
+    loop: true,
     autoplay: true,
-    margin:10,
+    margin: 10,
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
-    nav:false,
+    nav: true,
     autoplayHoverPause: false,
     items: 1,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
       },
-      600:{
-        items:1,
-        nav:false
+      600: {
+        items: 1,
+        nav: false
       },
-      1000:{
-        items:1,
-        nav:false
+      1000: {
+        items: 1,
+        nav: true
       }
     }
   });
 
-	// owl carousel
-	var majorCarousel = $('.js-carousel-1');
-	majorCarousel.owlCarousel({
-    loop:true,
+
+  // home slider
+  $('.testimony-slider').owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: false,
+    autoplayHoverPause: false,
+    items: 1,
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 1,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        nav: false
+      }
+    }
+  });
+
+  // owl carousel
+  var majorCarousel = $('.js-carousel-1');
+  majorCarousel.owlCarousel({
+    loop: true,
     autoplay: true,
     stagePadding: 7,
     margin: 20,
@@ -127,28 +134,28 @@
     nav: true,
     autoplayHoverPause: true,
     items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
       },
-      600:{
-        items:2,
-        nav:false
+      600: {
+        items: 2,
+        nav: false
       },
-      1000:{
-        items:3,
-        nav:true,
-        loop:false
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false
       }
-  	}
-	});
+    }
+  });
 
-	// owl carousel
-	var major2Carousel = $('.js-carousel-2');
-	major2Carousel.owlCarousel({
-    loop:true,
+  // owl carousel
+  var major2Carousel = $('.js-carousel-2');
+  major2Carousel.owlCarousel({
+    loop: true,
     autoplay: true,
     stagePadding: 7,
     margin: 20,
@@ -157,60 +164,60 @@
     nav: true,
     autoplayHoverPause: true,
     items: 4,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
       },
-      600:{
-        items:3,
-        nav:false
+      600: {
+        items: 3,
+        nav: false
       },
-      1000:{
-        items:4,
-        nav:true,
-        loop:false
+      1000: {
+        items: 4,
+        nav: true,
+        loop: false
       }
-  	}
-	});
+    }
+  });
 
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.element-animate').waypoint( function( direction ) {
+  var contentWayPoint = function () {
+    var i = 0;
+    $('.element-animate').waypoint(function (direction) {
 
-			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
-				
-				i++;
+      if (direction === 'down' && !$(this.element).hasClass('element-animated')) {
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+        i++;
 
-					$('body .element-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn element-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft element-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight element-animated');
-							} else {
-								el.addClass('fadeInUp element-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 100);
-					});
-					
-				}, 100);
-				
-			}
+        $(this.element).addClass('item-animate');
+        setTimeout(function () {
 
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
+          $('body .element-animate.item-animate').each(function (k) {
+            var el = $(this);
+            setTimeout(function () {
+              var effect = el.data('animate-effect');
+              if (effect === 'fadeIn') {
+                el.addClass('fadeIn element-animated');
+              } else if (effect === 'fadeInLeft') {
+                el.addClass('fadeInLeft element-animated');
+              } else if (effect === 'fadeInRight') {
+                el.addClass('fadeInRight element-animated');
+              } else {
+                el.addClass('fadeInUp element-animated');
+              }
+              el.removeClass('item-animate');
+            }, k * 100);
+          });
+
+        }, 100);
+
+      }
+
+    }, { offset: '95%' });
+  };
+  contentWayPoint();
 
 
 
